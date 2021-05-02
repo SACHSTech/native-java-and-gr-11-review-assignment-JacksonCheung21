9cb7be4393cbbf0a20dcd7a3b1f1c4cd84658354
Jackson Cheung <>
2021-05-02 22:19:38 +0000

    Review 1 initialized variables and created buffer reader

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,36 @@
 package gr11review.part1;
+import java.io*;
+
+/**
+* A program that prints the day of the year from given inputs from the user
+@author: J. Cheung
+*/
+
+public class Review1 {
+
+ public static void main(String[] args) throws IOException {
+ 
+  // Buffer Reader
+  BufferReader key = new BufferdReader(new InputStreamReader(Systen.in));
+
+  // Initializing Variables
+  int intMonth;
+  int intDays;
+  int intDayNumber;
+  int intCount;
+  int intTotal = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+}
