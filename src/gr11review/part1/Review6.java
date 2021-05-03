029b1e7948c39c9e1176be17fc3c0cb7378fa214
Jackson Cheung <>
2021-05-03 08:16:14 +0000

    fixes and final

--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -12,35 +12,32 @@ import java.text.DecimalFormat;
 public class Review6{
   public static void main(String[] args) throws IOException{
 
-    BufferedReader key = new BufferedReader(new InputStreamer(System.in));
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
     DecimalFormat df = new DecimalFormat("#,##0.00");
 
     // initializing variables
 
-    double dblItemPrice =1;
-    double dblTotalPrice = 0;
+    double dblItemPrice = 1;
+    double dblSubTotal = 0;
+    double dblTax;
+    double dblTotal;
 
     // Set up while loop if the input is not 0 loop will continue
-    while (dblItemPrice !=0){
-        System.out.println("Enter the price for an item: ");
+    while (dblItemPrice != 0){
+        System.out.print("Enter the price for an item: ");
         dblItemPrice = Double.parseDouble(key.readLine());
-        dblTotalPrice += dblItemPrice;
+        dblSubTotal = dblItemPrice + dblSubTotal;
     }
+    // Calculations
 
+    dblTax = dblSubTotal * 0.13;
+    dblTotal = dblSubTotal + dblTax;
 
-
-
-
-
-
-
-
-
-
-
-
-
+    // printing out the subtotal, tax, and total using decimal format 
+    System.out.println("Subtotal: $" + df.format(dblSubTotal));
+    System.out.println("Tax: $" + df.format(dblTax));
+    System.out.println("Total: $" + df.format(dblTotal));
 
   }
 }
