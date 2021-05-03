4e7081027ac47f2dc20e851b26b211d19ff60e52
Jackson Cheung <>
2021-05-03 16:32:54 +0000

    initializing variables

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -1,1 +1,38 @@
 package gr11review.part1;
+import java.io.*;
+
+/**
+* This program stiumlates 1000 pulls of a slot machine
+* 3 Random numbers between 0-8 are created each pull
+* It will then print out how many times a triple was pulled
+* @Author: J. Cheung
+*/
+
+public class Review8{
+  public static void main(String[] args) throws IOException {
+  
+  BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+  // initializing variables
+
+  int intslot;
+  int intslot2;
+  int intslot3;
+  int intCount;
+  int intTriple = 0;
+
+  // Genearing loop that will continue until the count goes to 1000
+
+  
+
+
+    
+
+
+
+
+
+
+
+  } 
+}
