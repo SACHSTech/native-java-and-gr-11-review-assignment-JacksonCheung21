22316677306c4d63c38c65d17c1e12ba9495726a
Jackson Cheung <>
2021-05-03 07:32:03 +0000

    final

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -19,18 +19,18 @@ public class Review4{
     int intNumItems = 0;
     int intCount;
     double dblPrice = 0;
+    double dblSubtotal = 0;
     double dblTax;
     double dblTotal;
-    double dblSubtotal = 0;
 
     // Asking user input for the number of items
-    System.out.print("How many items do you want to buy?");
+    System.out.print("How many items do you want to buy? ");
     intNumItems = Integer.parseInt(key.readLine());
 
     // For loop that will continue going until user has inputted the price of items
 
     for (intCount = 1; intCount<intNumItems+1; intCount++){
-        System.out.print("Enter the price for item " + intCount + ":");
+        System.out.print("Enter the price for item " + intCount + ": ");
         dblPrice = Double.parseDouble(key.readLine());
         dblSubtotal = dblPrice + dblSubtotal;
     }
