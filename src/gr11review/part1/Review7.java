890e6f9ef1f9adb996e1188bdb8dd5b3c3946c0f
Jackson Cheung <>
2021-05-03 08:28:24 +0000

    asking for user input (sentence)

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -1,1 +1,48 @@
 package gr11review.part1;
+
+import java.io*;
+
+/**
+* This program asks user to input a sentence
+* It will then count the # of characters, including spaces and "a"s in a sentence
+* It will also insert a dash for every odd numbered charater in a sentence
+* @author: J.Cheung
+*/
+
+public class Review7{
+  public static void main(String[] args) throw IOException{
+
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+    //initializing variables
+    String strSentence;
+    int intCharacters;
+    int intSpaces = 0;
+    int intLetterA = 0;
+    int intCount;
+    String strDashes = "";
+
+    // Asking user for input (sentence)
+    strSentence = key.readLine();
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
