59c41d5072d52ba1fb55f7631f85be0efab49b0d
Jackson Cheung <>
2021-05-03 00:42:03 +0000

    initialized variables and the menu options

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,1 +1,36 @@
 package gr11review.part1;
+import java.io.*;
+
+/**
+* program that prints out jokes according to user input
+* If user input number dont match then print "Invalid menu option"
+*@author: C.Jackson
+*/
+
+public class Review2{
+ public static void main(String[] args) throws IOException{
+  BufferedReader key = new Buffered(new InputStreamReader(System.in));
+
+  // Initializing Variables
+  int intMenuOption;
+
+  // prints option from 0,1,2,3 
+  System.out.println("0 - print a joke about your hair");
+  System.out.println("0 - print a joke about your feet");
+  System.out.println("0 - print a joke about your clothes");
+  System.out.println("0 - print a joke about your teacher");
+  System.out.print("Choose a menu option: ")
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+}
