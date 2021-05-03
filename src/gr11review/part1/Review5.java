db9c32cf6ae33847dd8c33ec48ebd1e678287b89
Jackson Cheung <>
2021-05-03 07:23:13 +0000

    final

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -7,19 +7,17 @@ import java.io.*;
 * @author: J, CHeung
 */
 
-
-
 public class Review5{
   public static void main(String[] args) throws IOException{
     BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
     //Initializing variables
-    double dblYearlyAmount = 0;
-    double dblCompoundInterestRate = 0;
-    dobule dblTargetAmount = 0;
     int intYear = 0;
-    double dblCompoundAmt;
-    double dblTotalMoney;
+    double dblTotalMoney = 0;
+    double dblTargetAmount;
+    double dblCompoundInterestRate;
+    double dblYearlyAmount;
+   
 
     // Asking user to input yearly amount, the compound interest rate and target amount
 
@@ -32,24 +30,16 @@ public class Review5{
     System.out.print("Enter the target amount: ");
     dblTargetAmount = Double.parseDouble(key.readLine());
 
-
     // While loop to run as long as the total money is less than the aimed amount
     while (dblTotalMoney < dblTargetAmount){
-      
-      //calculating the compound amount
-      dblCompoundAmt = ((DblTotalMoney + dblYearlyAmount)*(dblCompoundInterestRate/100));
-
-      // calculating the total money
-      dblTotalMoney += dblCompoundAmt+dblYearlyAmount;
-
-      // adding one to the counter for every run through
+      dblTotalMoney += dblYearlyAmount;
+      dblTotalMoney += (dblTotalMoney * (dblCompoundInterestRate / 100));
       intYear++;
-
     }
 
     // printing out the result 
 
-    System.out.println("The target amount will be in" + intYear + " years");
+    System.out.println("The target amount will be earned in " + intYear + " years.");
 
   }
 }
