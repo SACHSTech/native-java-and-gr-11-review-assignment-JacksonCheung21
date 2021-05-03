a391742675a46423bc97bbce9928511fd3cb3dac
Jackson Cheung <>
2021-05-03 04:53:40 +0000

    initialized variables and for loop for user input price of items

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,49 @@
 package gr11review.part1;
+
+import java.io.*;
+import java.text.*;
+
+/**
+* A program that asks how many items they would want to purchase. 
+* Which then will take the total price to print out total with tax
+* @author: J. Cheung
+*/
+
+public class Review4{
+  public static void main(String[] args) throws IOException{
+
+    BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+    Number numberFormat = new DecimalFormat("#,##0.00");
+
+    // initializing variables
+    int intNumItems;
+    int intCount;
+    double dblPrice;
+    double dblSubtotal;
+    double dblTax;
+    double dblTotal;
+
+    // Asking user input for the number of items
+    System.out.print("How many items do you want to buy?");
+    intNumItems = Integer.parseInt(key.readLine());
+
+    // For loop that will continue going until user has inputted the price of items
+
+    for (intCount = 1; intCount <= intNumItems; intCount++){
+        Systen.out.println("Enter the price for titem " +intCount+ ":");
+        dblPrice = Double.parseDouble(key.readLine());
+        dblSubtotal += dblPrice;
+    }
+  
+
+
+
+
+
+
+
+
+
+
+  }
+}
