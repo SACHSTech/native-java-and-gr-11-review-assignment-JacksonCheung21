4258b58eef485d10687f37235dff06094f7f9781
Jackson Cheung <>
2021-05-03 00:58:34 +0000

    case 1

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -16,10 +16,25 @@ public class Review2{
 
   // prints option from 0,1,2,3 
   System.out.println("0 - print a joke about your hair");
-  System.out.println("0 - print a joke about your feet");
-  System.out.println("0 - print a joke about your clothes");
-  System.out.println("0 - print a joke about your teacher");
-  System.out.print("Choose a menu option: ")
+  System.out.println("1 - print a joke about your feet");
+  System.out.println("2 - print a joke about your clothes");
+  System.out.println("3 - print a joke about your teacher");
+  System.out.print("Choose a menu option: ");
+
+  // user input number 
+  intMenuOption = Integer.parseInt(key.readLine());
+
+ // switch/cases 
+ switch (intMenuOption) { 
+   case 0:
+   // If the user input at intMenuOption is 0 it prints out joke about your hair
+   System.out.println("Why do bees have sticky hair?");
+   System.out.println("They always use honeycombs.");
+   break;
+
+   case 1:
+   // If the user input at intMenuOption is 1 it prints out a joke about your feet
+   
 
 
 
@@ -27,6 +42,7 @@ public class Review2{
 
 
 
+ }
 
 
 
