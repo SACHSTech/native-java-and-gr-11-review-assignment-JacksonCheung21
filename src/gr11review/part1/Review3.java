49fba626834a6af4aa01af4e25038c7f86ba9e0c
Jackson Cheung <>
2021-05-03 04:35:48 +0000

    final

--- src/gr11review/part1/Review3.java
+++ src/gr11review/part1/Review3.java
@@ -8,3 +8,25 @@ import java.io.*;
 * @author: J. Cheung
 */
 
+public class Review3 {
+  public static void main(String[] args) throws IOException {
+
+    // initialize variables
+    int intCountOdd;
+    int intCountDown;
+
+    // print odd numbers from 20-100
+    for(intCountOdd=20; intCountOdd < 100; intCountOdd++){
+      if(intCountOdd %2 !=0){
+        System.out.println(intCountOdd);
+      }
+    }
+    // blank line 
+    System.out.println("");
+
+    // print numbers decreasing from 29-2
+    for(intCountDown = 29; intCountDown >=2; intCountDown--){
+      System.out.println(intCountDown);
+    }
+  } 
+}
