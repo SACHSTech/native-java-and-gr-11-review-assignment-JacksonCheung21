c7e4640c8097d06a8a3c543b6f9dd33869ac2d75
Jackson Cheung <>
2021-05-03 16:38:55 +0000

    final

--- src/gr11review/part1/Review8.java
+++ src/gr11review/part1/Review8.java
@@ -15,7 +15,7 @@ public class Review8{
 
   // initializing variables
 
-  int intslot;
+  int intslot1;
   int intslot2;
   int intslot3;
   int intCount;
@@ -23,15 +23,25 @@ public class Review8{
 
   // Genearing loop that will continue until the count goes to 1000
 
-  
+  for (intCount = 0; intCount < 1000; intCount++){
 
+    // this will generate random numbers from 1 to 8
+    intslot1 = (int)(Math.random()*8+1);
+    intslot2 = (int)(Math.random()*8+1);
+    intslot3 = (int)(Math.random()*8+1);
 
-    
+    System.out.print(intslot1 + " ");
+    System.out.print(intslot2 + " ");
+    System.out.println(intslot3);
 
+    // Seeing if any triple been pulled
 
+    if (intslot1 == intslot2 && intslot1 == intslot3){
+      intTriple++;
+    }
 
-
-
+  }
+    System.out.println(intTriple);
 
 
   } 
