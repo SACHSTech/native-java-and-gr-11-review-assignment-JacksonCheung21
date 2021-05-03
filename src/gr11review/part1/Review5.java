eb02f10150367c9b3cae16f3577bcee175a896d7
Jackson Cheung <>
2021-05-03 06:47:05 +0000

    while loop and printing result

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -17,6 +17,9 @@ public class Review5{
     double dblYearlyAmount = 0;
     double dblCompoundInterestRate = 0;
     dobule dblTargetAmount = 0;
+    int intYear = 0;
+    double dblCompoundAmt;
+    double dblTotalMoney;
 
     // Asking user to input yearly amount, the compound interest rate and target amount
 
@@ -30,12 +33,23 @@ public class Review5{
     dblTargetAmount = Double.parseDouble(key.readLine());
 
 
+    // While loop to run as long as the total money is less than the aimed amount
+    while (dblTotalMoney < dblTargetAmount){
+      
+      //calculating the compound amount
+      dblCompoundAmt = ((DblTotalMoney + dblYearlyAmount)*(dblCompoundInterestRate/100));
 
+      // calculating the total money
+      dblTotalMoney += dblCompoundAmt+dblYearlyAmount;
 
+      // adding one to the counter for every run through
+      intYear++;
 
+    }
 
+    // printing out the result 
+
+    System.out.println("The target amount will be in" + intYear + " years");
 
   }
-
-
 }
