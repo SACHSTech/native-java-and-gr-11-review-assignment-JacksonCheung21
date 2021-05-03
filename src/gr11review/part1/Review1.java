7fcdf48c5fe585e45b96b80551bfabd18da4de3b
Jackson Cheung <>
2021-05-03 00:22:20 +0000

    final Review 1

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -6,9 +6,9 @@ import java.io.*;
 @author: J. Cheung
 */
 
-public class Review1 {
+public class Review1{
 
- public static void main(String[] args) throws IOException {
+ public static void main(String[] args) throws IOException{
  
   // Buffered Reader
   BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
@@ -16,27 +16,35 @@ public class Review1 {
   // Initializing Variables
   int intMonth;
   int intDays;
-  int intDayNumber;
+  int intDayofYear;
   int intCount;
   int intTotal = 0;
 
   // User input for month and day
-  System.out.println("Enter the month numbedr: ");
+  System.out.print("Enter the month number: ");
   intMonth = Integer.parseInt(key.readLine());
-  System.out.println("Enter the day number: ");
+
+  System.out.print("Enter the day number: ");
   intDays = Integer.parseInt(key.readLine());
 
+  // Calculating for the amount of days and month intTotal
+  for (intCount = 1; intCount < intMonth; intCount++){
+    if (intCount == 2){
+      intTotal += 28;
+    }
+    else if (intCount == 4 || intCount == 6 || intCount == 9 || intCount == 11){
+      intTotal += 30; 
+    }
+    else {
+      intTotal += 31;
+    }
 
+  }
 
+  // Adding day number and month total to get # of day in the year
+  intDayofYear = intTotal + intDays;
 
-
-
-
-
-
-
-
-
+  // Result
+  System.out.println(intDayofYear);
  }
-
 }
